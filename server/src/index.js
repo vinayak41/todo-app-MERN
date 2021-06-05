693a1fe908366e2606cd8cf5c6fcc7b1216a3b6b
vinayak Khandekar <vinayakkhandekar41@gmail.com>
2021-06-05 11:39:34 +0530

    connect database

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,18 @@
 const express = require('express')   
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect(
+    `mongodb+srv://vinayak:vinayak123@cluster0.jpxqn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+    {
+        useNewUrlParser: true, 
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    }
+).then(() => {
+    console.log('Database Connected')
+})
 const todoRoutes = require('./routes/todo');
 
 app.use('/todo', todoRoutes);
