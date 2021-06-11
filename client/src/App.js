c1e6dfba8ad70dc7ee524fa96b928ee703e13146
vinayak Khandekar <khandekarvinayak41@gmail.com>
2021-06-11 19:22:00 +0530

    home component

--- client/src/App.js
+++ client/src/App.js
@@ -4,21 +4,31 @@ import TodoList from "./components/TodoList/TodoList";
 import Login from "./components/Login/Login";
 import SignUp from "./components/SignUp/SignUp";
 import Header from "./components/Header/Header";
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 
 function App() {
   return (
     <div className="app">
       <Header />
-      <Router>
-        <Switch>
-          <Route exact path="/" ><Home /></Route>
-          <Route path="/login" ><Login /></Route>
-          <Route path="/signup" ><SignUp /></Route>
-          <Route path="/todo_list" ><TodoList /></Route>
-        </Switch>
-      </Router>
+      <div className="app-body">
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/signup">
+              <SignUp />
+            </Route>
+            <Route path="/todos">
+              <TodoList />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
     </div>
   );
 }
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,12 +1,17 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Home.css";
 
 export default function Home() {
-    return (
-        <div style={{'margin-top': '10rem'}}>
-            <Link to="/login">Login</Link>
+  return (
+    <div className="home">
+      {/* <Link to="/login">Login</Link>
             <Link to="/signup">Login</Link>
-            <Link to="/todo_list">Login</Link>
-        </div>
-    )
+            <Link to="/todo_list">Login</Link> */}
+      <p>Todo app can help you to make list of your tasks</p>
+      <button>
+        <Link to="/login">Get Started</Link>
+      </button>
+    </div>
+  );
 }
