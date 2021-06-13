b28676c9a8d18a4ca84e54bbf25f82b2f0b57334
vinayak Khandekar <khandekarvinayak41@gmail.com>
2021-06-13 23:31:52 +0530

    update models

--- server/src/models/todo.js
+++ server/src/models/todo.js
@@ -4,6 +4,7 @@ const todoSchema = mongoose.Schema({
     id: { type: Number, required: true},
     text: { type: String, required: true},
     isDone: { type: Boolean, required: true, default: false},
+    author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true}
 })
 
 module.exports = mongoose.model('Todo', todoSchema);
--- server/src/models/user.js
+++ server/src/models/user.js
@@ -3,12 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true},
     email: { type: String, required: true, unique: true},
-    password: { type: String, required: true},
-    todos: [
-        {
-            type: mongoose.Schema.Types.ObjectId, ref: "Todo", required: true
-        }
-    ]
+    password: { type: String, required: true}
 });
 
 module.exports = mongoose.model('User', userSchema);
