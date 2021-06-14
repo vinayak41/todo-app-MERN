61aae9b3bdca1910945845a99cfbf6013a2c1e5a
vinayak Khandekar <khandekarvinayak41@gmail.com>
2021-06-14 23:03:22 +0530

    fix signup error

--- server/src/controllers/user.js
+++ server/src/controllers/user.js
@@ -3,36 +3,32 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 exports.signup = async (req, res) => {
-  User.find({ email: req.body.email }).exec((error, user) => {
+  User.findOne({ email: req.body.email }).exec((error, user) => {
     if (error) res.json({ error });
     if (user) {
       res.status(400).json({ message: "Email already in use" });
-    } else {
-      const hashPassword = bcrypt.hashSync(req.body.password, 10, (err, hash) => {
-        console.log('hash')
-        if (err) res.json({ error: err });
-        if (hash) {
-          return hash;
-        }
-      });
-
-      const newUser = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: hashPassword,
-      });
-    
-      newUser.save((error, user) => {
-        console.log('save')
-        if (error) {
-          res.json({ error });
-        }
-        if (user) {
-          res.json({ meaasage: "signup seccessful" });
-        }
-        res.json({ message: "something went wrong" });
-      });
     }
+    const hashPassword = bcrypt.hashSync(req.body.password, 10, (err, hash) => {
+      if (err) res.json({ error: err });
+      if (hash) {
+        return hash;
+      }
+    });
+  
+    const newUser = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: hashPassword,
+    });
+  
+    newUser.save((error, user) => {
+      if (error) {
+        res.json({ error });
+      }
+      if (user) {
+        res.json({ meaasage: "signup seccessful" });
+      }
+    });
   });
 };
 
