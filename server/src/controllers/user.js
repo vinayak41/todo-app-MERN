8753aff90cb860c5261fe1ca29e0e78a2ce0e643
vinayak Khandekar <khandekarvinayak41@gmail.com>
2021-06-14 22:49:19 +0530

    fix error

--- server/src/controllers/user.js
+++ server/src/controllers/user.js
@@ -3,50 +3,58 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 exports.signup = async (req, res) => {
-  const user = await User.find({ email: req.body.email }); //change later to callback
-  if (user) {
-    res.status(400).json({ message: "Email already in use" });
-  }
+  User.find({ email: req.body.email }).exec((error, user) => {
+    if (error) res.json({ error });
+    if (user) {
+      res.status(400).json({ message: "Email already in use" });
+    } else {
+      const hashPassword = bcrypt.hashSync(req.body.password, 10, (err, hash) => {
+        console.log('hash')
+        if (err) res.json({ error: err });
+        if (hash) {
+          return hash;
+        }
+      });
 
-  const hashPassword = bcrypt.hashSync(req.body.password, 10, (err, hash) => {
-    if (err) res.json({ error: err });
-    if (hash) {
-      console.log(hash);
-      return hash;
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: hashPassword,
+      });
+    
+      newUser.save((error, user) => {
+        console.log('save')
+        if (error) {
+          res.json({ error });
+        }
+        if (user) {
+          res.json({ meaasage: "signup seccessful" });
+        }
+        res.json({ message: "something went wrong" });
+      });
     }
-  });
-  const newUser = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: hashPassword,
-  });
-
-  newUser.save((error, user) => {
-    if (error) {
-      res.json({ error });
-      return;
-    }
-    if (user) res.json({ user });
   });
 };
 
 exports.login = (req, res) => {
   User.findOne({ email: req.body.email }).exec((error, user) => {
-    if (error) res.json({error});
+    if (error) res.json({ error });
     if (user) {
       bcrypt.compare(req.body.password, user.password, (error, result) => {
         if (error) {
-          console.log(error)
-          res.status(400).json({ error })
-        };
+          console.log(error);
+          res.status(400).json({ error });
+        }
         if (result) {
-          const token = jwt.sign({_id: user._id}, process.env.JWT_SECRET, { expiresIn: '1h'});
-          const jwtreturn = jwt.verify(token, process.env.JWT_SECRET)
-          res.status(200).json({token})
+          const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+            expiresIn: "1h",
+          });
+          const jwtreturn = jwt.verify(token, process.env.JWT_SECRET);
+          res.status(200).json({ token });
         } else {
-          res.status(400).json({message: "Invalid email/password"})
-        };
+          res.status(400).json({ message: "Invalid email/password" });
+        }
       });
-    } else res.status(400).json({message: error})
+    } else res.status(400).json({ message: error });
   });
 };
