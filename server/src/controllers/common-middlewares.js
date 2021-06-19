fed5ed7938ac86c3ebe73d950b83123602e0643b
vinayak Khandekar <khandekarvinayak41@gmail.com>
2021-06-19 22:35:56 +0530

    handle jwt error

--- server/src/controllers/common-middlewares.js
+++ server/src/controllers/common-middlewares.js
@@ -1,13 +1,14 @@
-const jwt = require('jsonwebtoken')
-
+const jwt = require("jsonwebtoken");
 
 exports.requireLogin = (req, res, next) => {
-    if(req.headers.authorization) {
-        const token = req.headers.authorization.split(" ")[1];
-        jwt.verify(token, process.env.JWT_SECRET, (error, decoded) => {
-            if(error) res.status(400).json({error})
-            req.userId = decoded._id;
-        })
-    }
-    next();
-} 
+  if (req.headers.authorization) {
+    const token = req.headers.authorization.split(" ")[1];
+    jwt.verify(token, process.env.JWT_SECRET, (error, decoded) => {
+      if (error) res.status(400).json({ error });
+      if (decoded) {
+        req.userId = decoded._id;
+        next();
+      }
+    });
+  }
+};
