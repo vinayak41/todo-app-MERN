74124b0b192388f27b6e73d42fc7d0828ef5b6d3
vinayak Khandekar <khandekarvinayak41@gmail.com>
2021-06-19 22:26:20 +0530

    fix mongoose error

--- server/src/index.js
+++ server/src/index.js
@@ -20,7 +20,8 @@ app.use(bodyParser.json());
 
 mongoose
   .connect(
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jpxqn.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+    // `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jpxqn.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&ssl=true`,
+    `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-shard-00-00.jpxqn.mongodb.net:27017,cluster0-shard-00-01.jpxqn.mongodb.net:27017,cluster0-shard-00-02.jpxqn.mongodb.net:27017/${process.env.DB_NAME}?ssl=true&replicaSet=atlas-ny7se5-shard-0&authSource=admin&retryWrites=true&w=majority`,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
@@ -29,6 +30,9 @@ mongoose
   )
   .then(() => {
     console.log("Database Connected");
+  })
+  .catch(error => {
+    console.log(error)
   });
 
 app.use("/todo", todoRoutes);
