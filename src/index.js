cf64d2c472ef2a7ed0396ce10d203701d13f549f
vinayak Khandekar <khandekarvinayak41@gmail.com>
2021-06-23 23:40:04 +0530

    update for deploy

--- src/index.js
+++ src/index.js
@@ -40,4 +40,13 @@ mongoose
 app.use("/todo", todoRoutes);
 app.use("/user", userRoutes);
 
+
+if( process.env.NODE_ENV == "production"){
+  app.use(express.static("client/build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
+
 app.listen(PORT);
