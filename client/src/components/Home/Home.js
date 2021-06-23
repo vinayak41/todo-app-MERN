8af1305ccc63b04ab65c2f2309c0b4f18a6d349f
vinayak Khandekar <khandekarvinayak41@gmail.com>
2021-06-23 12:31:23 +0530

    add image to home page

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,17 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Home.css";
+import todoImg from "../../assets/images/todo.png"
 
 export default function Home() {
   return (
     <div className="home">
-      {/* <Link to="/login">Login</Link>
-            <Link to="/signup">Login</Link>
-            <Link to="/todo_list">Login</Link> */}
-      <p>Todo app can help you to make list of your tasks</p>
-      <button>
-        <Link to="/login">Get Started</Link>
-      </button>
+      <div className="left">
+        <p>Todo app can help you to make list of your tasks</p>
+        <button>
+          <Link to="/login">Get Started</Link>
+        </button>
+      </div>
+      <div className="img-wrapper">
+        <img src={todoImg} alt="todo-img" />
+      </div>
     </div>
   );
 }
