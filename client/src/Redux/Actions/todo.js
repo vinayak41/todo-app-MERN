dc0ce062031a3e4e6d7fbb659ce027f0309be2e4
vinayak Khandekar <khandekarvinayak41@gmail.com>
2021-06-24 12:34:49 +0530

    make it better

--- client/src/Redux/Actions/todo.js
+++ client/src/Redux/Actions/todo.js
@@ -1,6 +1,6 @@
 import { FETCH_TODOS_SUCCESS, DELETE_TODO, DONE_TODO, UNDONE_TODO, ADD_TODO_SUCCESS} from '../TypeConstants/typeConstants';
 import axios from 'axios';
-const api = `${process.env.APP_URL || 'http://localhost:8000' }/todo`;
+const api = `${"https://todo-app-mern41.herokuapp.com" || 'http://localhost:8000' }/todo`;
 
 export const addTodo = (text) => {
     return (dispatch) => {
--- client/src/Redux/Actions/user.js
+++ client/src/Redux/Actions/user.js
@@ -1,7 +1,6 @@
 import {LOGIN_SUCCESS, SIGNUP_SUCCESS, SIGNUP_FAIL, LOGIN_FAIL, IS_LOGIN_TRUE, IS_LOGIN_FALSE, LOGOUT, RESET_ERR_MSG} from '../TypeConstants/typeConstants';
 import axios from 'axios';
-const api = `${process.env.APP_URL || 'http://localhost:8000'}/user`
-console.log(process.env.APP_URL)
+const api = `${"https://todo-app-mern41.herokuapp.com" || 'http://localhost:8000'}/user`
 
 export const login = (email, password) => {
     return (dispatch) => {
