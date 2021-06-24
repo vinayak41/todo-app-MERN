ec531e73cbfcc34ec52708fe1628f6d0e91a836c
vinayak Khandekar <khandekarvinayak41@gmail.com>
2021-06-24 11:58:32 +0530

    make it better

--- client/src/Redux/Actions/todo.js
+++ client/src/Redux/Actions/todo.js
@@ -1,6 +1,6 @@
 import { FETCH_TODOS_SUCCESS, DELETE_TODO, DONE_TODO, UNDONE_TODO, ADD_TODO_SUCCESS} from '../TypeConstants/typeConstants';
 import axios from 'axios';
-const api = `${process.env.APP_URL}/todo`;
+const api = `${process.env.APP_URL || 'http://localhost:8000' }/todo`;
 
 export const addTodo = (text) => {
     return (dispatch) => {
